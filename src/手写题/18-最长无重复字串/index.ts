7a0ea435a14f76760d685b98a2bfca7a4b91f5b9
程明旺 <cmw1002113844@gmail.com>
2022-10-27 16:46:11 +0800

    最长无重复子串

---
+++ src/手写题/18-最长无重复字串/index.ts
@@ -0,0 +1,24 @@
+﻿function maxSubStr(str: string): string {
+  if (str.length <= 1) {
+    return str
+  }
+  let left = 0
+  let right = 1
+  let curStr = str[0]
+  let maxStr = ''
+  for (; right < str.length; right++) {
+    const index = curStr.indexOf(str[right])
+    if (index >= 0) {
+      left = index + 1
+      continue
+    } else {
+      curStr = str.slice(left, right + 1)
+    }
+    if (curStr.length > maxStr.length) {
+      maxStr = curStr
+    }
+  }
+  return maxStr
+}
+const res = maxSubStr('abcdabcde')
+console.log(res)
